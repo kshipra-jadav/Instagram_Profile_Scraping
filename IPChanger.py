d73e07c808bc581875677ecd3b752a3886971350
kshipra-jadav <jadavkshipra2@gmail.com>
2024-12-09 10:14:47 +0530

    Initial commit

---
+++ IPChanger.py
@@ -0,0 +1,2 @@
+class IPChanger:
+    pass
---
+++ InstagramScraper.py
@@ -0,0 +1,40 @@
+import requests
+import user_agent
+from urllib.parse import urlparse
+
+USER_BASEURL = "https://www.instagram.com/api/v1/users/web_profile_info"
+X_IG_APP_ID = "936619743392459"
+
+class InstagramScraper:
+    def __init__(self) -> None:
+        pass
+
+    def __scrape_user(self, username: str) -> None:
+        params = {'username': username}
+        headers = {
+            'User-Agent': user_agent.generate_user_agent(),
+            'X-IG-App-ID': X_IG_APP_ID
+        }
+
+        req = requests.models.PreparedRequest()
+
+        req.prepare_url(url=USER_BASEURL, params=params)
+
+        res = requests.get(req.url, headers=headers)
+        print(res.json())
+
+
+
+    def scrape_user_from_url(self, user_url: str):
+        url = urlparse(user_url)
+        username = url.path.replace("/", "")
+        self.__scrape_user(username)
+
+    def scrape_user_from_username(self, username: str):
+        self.__scrape_user(username)
+
+
+# testing purposes only
+if __name__ == '__main__':
+    igscr = InstagramScraper()
+    igscr.scrape_user_from_url("https://www.instagram.com/leomessi/?hl=en")
