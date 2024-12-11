99dbcf89bc6e2d3cb83a207b9d6094d2a576105d
kshipra-jadav <jadavkshipra2@gmail.com>
2024-12-11 08:47:08 +0530

    refactor some code in IPChanger.py

--- InstagramScraper.py
+++ InstagramScraper.py
@@ -2,10 +2,11 @@ import requests
 import user_agent
 from urllib.parse import urlparse
 
-USER_BASEURL = "https://www.instagram.com/api/v1/users/web_profile_info"
-X_IG_APP_ID = "936619743392459"
 
 class InstagramScraper:
+    USER_BASEURL = "https://www.instagram.com/api/v1/users/web_profile_info"
+    X_IG_APP_ID = "936619743392459"
+
     def __init__(self) -> None:
         pass
 
@@ -13,12 +14,12 @@ class InstagramScraper:
         params = {'username': username}
         headers = {
             'User-Agent': user_agent.generate_user_agent(),
-            'X-IG-App-ID': X_IG_APP_ID
+            'X-IG-App-ID': self.X_IG_APP_ID
         }
 
         req = requests.models.PreparedRequest()
 
-        req.prepare_url(url=USER_BASEURL, params=params)
+        req.prepare_url(url=self.USER_BASEURL, params=params)
 
         res = requests.get(req.url, headers=headers)
         print(res.json())
