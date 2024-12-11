cf1979c7673463e6143f4ad71fbd563d859bfbd6
kshipra-jadav <jadavkshipra2@gmail.com>
2024-12-11 07:01:30 +0530

    Add Proxy List Scraping Functionality

--- IPChanger.py
+++ IPChanger.py
@@ -1,2 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pp
+
+PROXIES_WEBSITE = 'https://free-proxy-list.net/'
+
 class IPChanger:
-    pass
+    def __init__(self):
+        pass
+
+    def scrape_proxies(self):
+        content = requests.get(PROXIES_WEBSITE).content
+        soup = BeautifulSoup(content, features='html.parser')
+
+        ip_addresses = []
+
+        tbody = soup.find('table').find('tbody')
+
+        for tr in tbody.find_all('tr'):
+            ip_addresses.append(tr.find('td').text)
+
+        pp(ip_addresses)
+
+
+
+# for testing purpose only
+if __name__ == '__main__':
+    ipc = IPChanger()
+    ipc.scrape_proxies()
